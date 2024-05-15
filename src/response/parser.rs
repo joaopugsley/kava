ffc961531f469ca05d9073821a87bc261df29b23
joaopugsley <joaopugsleyy@gmail.com>
2024-05-15 14:28:50 -0300

    feat: add support for parsing bulk strings

--- src/response/parser.rs
+++ src/response/parser.rs
@@ -7,6 +7,7 @@ use super::response::Response;
 pub fn parse_message(buffer: BytesMut) -> Result<(Response, usize), String> {
     match buffer[0] as char {
         '+' => parse_simple_string(buffer),
+        '$' => parse_bulk_string(buffer),
         _ => Err(format!("Not a known value type {:?}", buffer)),
     }
 }
@@ -17,6 +18,28 @@ fn parse_simple_string(buffer: BytesMut) -> Result<(Response, usize), String> {
         return Ok((Response::SimpleString(string), len + 1));
     }
     Err(format!("Invalid string {:?}", buffer))
+}
+
+fn parse_bulk_string(buffer: BytesMut) -> Result<(Response, usize), String> {
+    let (bulk_str_length, bytes_consumed) = if let Some((buf, len)) = read_until_crlf(&buffer[1..])
+    {
+        if let Ok(bulk_str_length) = parse_int(buf) {
+            (bulk_str_length, len + 1)
+        } else {
+            return Err(format!("Invalid bulk string format {:?}", buffer));
+        }
+    } else {
+        return Err(format!("Invalid bulk string format {:?}", buffer));
+    };
+
+    let end_of_bulk_str = bytes_consumed + bulk_str_length as usize;
+    let total_parsed = end_of_bulk_str + 2;
+
+    if let Ok(string) = String::from_utf8(buffer[bytes_consumed..end_of_bulk_str].to_vec()) {
+        return Ok((Response::BulkString(string), total_parsed));
+    }
+
+    Err(format!("Invalid bulk string format {:?}", buffer))
 }
 
 fn read_until_crlf(buffer: &[u8]) -> Option<(&[u8], usize)> {
