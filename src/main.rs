4fa5825042fcf4ea985b22a8702d1a5809b1cda9
joaopugsley <joaopugsleyy@gmail.com>
2024-05-16 05:05:27 -0300

    feat: add database module

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
+use std::sync::Arc;
+
+use database::Database;
 use resp::{parse_command, ResponseHandler, Value};
 use tokio::net::{TcpListener, TcpStream};
 
 mod commands;
+mod database;
 mod resp;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let db = Arc::new(Database::new());
 
     loop {
         let stream = listener.accept().await;
 
         match stream {
             Ok((stream, _)) => {
-                tokio::spawn(async move { handle_connection(stream).await });
+                let _db = Arc::clone(&db);
+                tokio::spawn(async move { handle_connection(stream, _db).await });
             }
             Err(err) => {
                 println!("Error: {}", err);
@@ -22,7 +28,7 @@ async fn main() {
     }
 }
 
-async fn handle_connection(stream: TcpStream) {
+async fn handle_connection(stream: TcpStream, db: Arc<Database>) {
     let mut handler = ResponseHandler::new(stream);
 
     loop {
