088db3325808d5c0b20a2775c0430bf3eb6a9d23
joaopugsley <joaopugsleyy@gmail.com>
2024-05-16 03:55:01 -0300

    docs: add documentation link to ping command

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,5 +1,6 @@
 use crate::resp::Value;
 
+// https://redis.io/docs/latest/commands/ping/
 pub fn ping(args: Vec<String>) -> Value {
     if args.len() > 0 {
         return Value::BulkString(format!("PONG \"{}\"", args.join(" ")));
