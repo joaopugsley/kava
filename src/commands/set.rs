141cd2b19d69e8ca4bd7de7e277df5cbc7c19bef
joaopugsley <joaopugsleyy@gmail.com>
2024-05-18 20:01:29 -0300

    feat: add missing options to 'set' command

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -22,8 +22,28 @@ pub async fn set(args: Vec<String>, db: &Arc<Database>) -> Value {
 
     let mut options = SetOptions::default();
 
+    // expiration time in milliseconds
     if let Some(px) = args::get::<u64>(&args, "px") {
         options.expiry_in_ms = px;
+    }
+
+    // expiration time in seconds
+    if let Some(ex) = args::get::<u64>(&args, "ex") {
+        options.expiry_in_ms = ex * 1000;
+    }
+
+    // only set if key does not exist
+    if args::some(&args, "nx") {
+        if db.get(key.to_string()).await.is_some() {
+            return Value::NullBulkString();
+        }
+    }
+
+    // only set if key already exists
+    if args::some(&args, "xx") {
+        if db.get(key.to_string()).await.is_none() {
+            return Value::NullBulkString();
+        }
     }
 
     let _ = db.set(key.clone(), value.clone(), options).await;
