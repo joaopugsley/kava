19f97afc229063c5582cd0ba31b5dc0453d88165
joaopugsley <joaopugsleyy@gmail.com>
2024-05-18 19:45:20 -0300

    refactor: utilize args module for parsing options in set command

--- src/commands/set.rs
+++ src/commands/set.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use crate::{
+    args,
     database::{Database, SetOptions},
     resp::Value,
 };
@@ -20,20 +21,9 @@ pub async fn set(args: Vec<String>, db: &Arc<Database>) -> Value {
     let value = args.get(1).unwrap_or(&default_value);
 
     let mut options = SetOptions::default();
-    let mut current_arg = 2;
-    while let Some(arg) = args.get(current_arg) {
-        match arg.to_lowercase().as_str() {
-            "px" => {
-                current_arg += 1;
-                if let Some(expiry) = args.get(current_arg) {
-                    if let Ok(expiry_in_ms) = expiry.parse::<u64>() {
-                        options.expiry_in_ms = expiry_in_ms;
-                    }
-                }
-            }
-            _ => {}
-        }
-        current_arg += 1;
+
+    if let Some(px) = args::get::<u64>(&args, "px") {
+        options.expiry_in_ms = px;
     }
 
     let _ = db.set(key.clone(), value.clone(), options).await;
