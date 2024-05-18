a981fb65a11572a1afad457967159f613e0ab40c
joaopugsley <joaopugsleyy@gmail.com>
2024-05-18 20:01:17 -0300

    feat: create 'some' function on args module

--- src/args.rs
+++ src/args.rs
@@ -7,3 +7,8 @@ pub fn get<T: FromStr>(args: &[String], name: &str) -> Option<T> {
         .and_then(|i| args.get(i + 1))
         .and_then(|arg| arg.parse::<T>().ok())
 }
+
+pub fn some(args: &[String], name: &str) -> bool {
+    let name = name.to_lowercase();
+    args.iter().any(|arg| *arg.to_lowercase() == name)
+}
